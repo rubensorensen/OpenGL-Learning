45b7145e4286ec123a2217f3c972d48118aa85e1
Ruben Sørensen <ruben.sorensen98@gmail.com>
2021-10-03 23:22:33 +0200

    Some changes

--- src/Application.cpp
+++ src/Application.cpp
@@ -25,20 +25,30 @@ int main()
     if (glewInit() != GLEW_OK)
         std::cout << "Error on glewInit" << std::endl;
 
-    std::cout << "OpenGL Version: " <<glGetString(GL_VERSION) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    float positions[2 * 3] {
+            -0.5f, -0.5f,
+             0.0f,  0.5f,
+             0.5f, -0.5f
+    };
+
+    uint32_t buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2d(-0.5f, -0.5f);
-        glVertex2d( 0.0f,  0.5f);
-        glVertex2d( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
